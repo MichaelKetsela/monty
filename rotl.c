5df5f7f88d867b19f4d6206f74e4b64b9af164bc
MichaelKetsela <michaelketsela837@gmail.com>
2022-12-14 21:46:49 +0300

    fix

--- rotl.c
+++ rotl.c
@@ -7,7 +7,7 @@
 *
 * Return: nothing
 */
-void i(stack_t **head, __attribute__((unused)) unsigned int line_counter)
+void i_rotl(stack_t **head, __attribute__((unused)) unsigned int line_counter)
 {
 stack_t *curr = *head, *aux;
 
